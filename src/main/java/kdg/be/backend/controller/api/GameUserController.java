6e035c71f12c582a47cb6eed15e99cd3cee75f4f
Yannick Vandenbulcke <Yannick.Vandenbulcke@student.kdg.be>
2024-11-27 15:04:43 +0100

    Added ResponseEntity<String> to return info to the frontend

--- src/main/java/kdg/be/backend/controller/api/GameUserController.java
+++ src/main/java/kdg/be/backend/controller/api/GameUserController.java
@@ -3,6 +3,7 @@ package kdg.be.backend.controller.api;
 import kdg.be.backend.controller.dto.GameUserDTO;
 import kdg.be.backend.domain.GameUser;
 import kdg.be.backend.service.GameUserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,14 +23,17 @@ public class GameUserController {
     }
 
     @PostMapping("/user")
-    public void createGameUser(@RequestBody GameUserDTO gameUserDTO) {
+    public ResponseEntity<String> createGameUser(@RequestBody GameUserDTO gameUserDTO) {
         if (gameUserDTO == null || gameUserDTO.getId() == null || gameUserDTO.getUsername() == null) {
             logger.warning("Invalid game user data");
+            return ResponseEntity.badRequest().body("Invalid game user data");
         } else if (gameUserService.gameUserExists(gameUserDTO.getId())){
             logger.info("Game user already exists");
+            return ResponseEntity.badRequest().body("Game user already exists");
         } else {
             gameUserService.createGameUser(gameUserDTO);
             logger.info("Game user " + gameUserDTO.getUsername() + " created");
+            return ResponseEntity.ok("Game user " + gameUserDTO.getUsername() + " created");
         }
 
     }
