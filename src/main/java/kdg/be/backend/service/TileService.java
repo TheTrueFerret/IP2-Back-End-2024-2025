c9b7a1ccfd36775d4c03f23d9107c528d8ba1bac
Yannick Vandenbulcke <Yannick.Vandenbulcke@student.kdg.be>
2024-11-29 11:33:58 +0100

    Added TileService

---
+++ src/main/java/kdg/be/backend/service/TileService.java
@@ -0,0 +1,24 @@
+package kdg.be.backend.service;
+
+import kdg.be.backend.domain.Tile;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+
+@Service
+public class TileService {
+
+
+    public boolean checkTileSet(LinkedList<Tile> tiles) {
+        boolean valid = true;
+        for (int i = 0; i < tiles.size(); i++) {
+            if (!tiles.getFirst().equals(tiles.get(i))) {
+                if (tiles.get(i).getNumberValue() != tiles.get(i - 1).getNumberValue() + 1) {
+                    return false;
+                }
+            }
+        }
+
+        return valid;
+    }
+}
