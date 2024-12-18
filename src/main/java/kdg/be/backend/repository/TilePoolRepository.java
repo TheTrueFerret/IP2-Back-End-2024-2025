c4764bd79532121d16c0f03fd7c65067728216c4
Lewis Ion <lewis.ion@student.kdg.be>
2024-12-18 16:50:51 +0100

    review comments toegepast

--- src/main/java/kdg/be/backend/repository/TilePoolRepository.java
+++ src/main/java/kdg/be/backend/repository/TilePoolRepository.java
@@ -1,11 +1,8 @@
 package kdg.be.backend.repository;
 
-import kdg.be.backend.domain.Game;
 import kdg.be.backend.domain.TilePool;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface TilePoolRepository extends JpaRepository<TilePool, UUID> {
