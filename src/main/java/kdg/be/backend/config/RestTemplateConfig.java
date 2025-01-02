51d47d6f51e8ddf73d6008fb53eec2f926bac2cc
Chahid Ahdour <chahid.ahdour@student.kdg.be>
2025-01-02 13:12:16 +0100

    config edit

--- src/main/java/kdg/be/backend/config/RestTemplateConfig.java
+++ src/main/java/kdg/be/backend/config/RestTemplateConfig.java
@@ -1,10 +1,12 @@
 package kdg.be.backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
