8387368a728a23aaf3139dd6c29e12ede58bc2bf
Yannick Vandenbulcke <Yannick.Vandenbulcke@student.kdg.be>
2025-01-04 17:42:49 +0100

    Added a little funny thing that was needed but not communicated love this!

--- src/test/java/kdg/be/backend/controller/api/PredictionControllerTest.java
+++ src/test/java/kdg/be/backend/controller/api/PredictionControllerTest.java
@@ -1,9 +1,11 @@
 package kdg.be.backend.controller.api;
 
+import kdg.be.backend.TestContainerIPConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Import(TestContainerIPConfiguration.class)
 @AutoConfigureMockMvc
 class PredictionControllerTest {
 
